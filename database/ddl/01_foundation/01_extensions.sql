4a00a004045728f7e3ed9bdbbf075d06cd7db612
Sandeep Kumar Panda <sandeeppanda22@gmail.com>
2026-06-15 10:26:42 +0530

    Add PostgreSQL foundation extensions

---
+++ database/ddl/01_foundation/01_extensions.sql
@@ -0,0 +1,8 @@
+-- =====================================================
+-- NSS ERP
+-- Module: Foundation
+-- File: 01_extensions.sql
+-- Version: 1.0
+-- =====================================================
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
